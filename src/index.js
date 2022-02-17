18d8a77880fc7d2dca5cf0708f2f19e4139a10e2
Damon <damondias@gmail.com>
2022-02-17 14:31:30 -0400

    feat: add server and Mongo database

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import router from './routes/index.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.use(express.json())
+app.use(cors());
+app.use(router)
+
+app.listen(process.env.PORT, ()=> {
+    console.log(`Rodando API Drivencracy em http://localhost:${process.env.PORT}`);
+});
