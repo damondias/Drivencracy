112fee77efbf395cd7c8ff7e95a5406394ab1fdf
Damon <damondias@gmail.com>
2022-02-20 03:06:34 -0400

    fix: joi's schemas required

--- src/schemas/choiceSchema.js
+++ src/schemas/choiceSchema.js
@@ -1,8 +1,8 @@
 import joi from "joi";
 
 const choiceSchema = joi.object({
-  title: joi.string()/*.required()*/,
-  poolId: joi.string()/*.required()*/,
+  title: joi.string().required(),
+  poolId: joi.string().required(),
 });
 
 export default choiceSchema;
